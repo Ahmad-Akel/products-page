9450b212013368a3b27bae55846137ba9bdbe433
ahmad <ahmad.oudai1999@gmail.com>
2024-09-02 15:52:31 +0200

    Initialize project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-8 bg-gray-100 min-h-screen">
+    <h1 class="text-4xl font-bold text-blue-600">Products Page</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
